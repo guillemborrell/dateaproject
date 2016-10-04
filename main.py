ec8bbc354603cca7501744784f4807da1f8f5ab3
Guillem Borrell <guillemborrell@gmail.com>
2016-10-04 12:46:31 +0200

    The project now shows the initial page.

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START app]
 import logging
+import jinja2
+import os
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 from flask import Flask
 
@@ -22,13 +15,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def main():
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    return template.render()
 
 
 @app.errorhandler(500)
 def server_error(e):
-    # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-# [END app]
+
